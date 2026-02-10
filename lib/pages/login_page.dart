b9ca26f45f04f171684e2d62bfd834c87b397664
Jiel109 <168947664+Jiel109@users.noreply.github.com>
2026-02-10 16:01:54 +0800

    Update #6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:popcom/auth/auth_service.dart';
+import 'dart:math';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,13 +9,33 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   // get auth service
   final authService = AuthService();
 
   // text controllers
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  // animation controller
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   // login button pressed
   void login() async {
@@ -40,96 +61,132 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return SingleChildScrollView(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-            ),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minHeight: constraints.maxHeight),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    //logo
-                    Image.asset(
-                      'lib/assets/images/popcom logo.png',
-                      height: 120,
-                      fit: BoxFit.contain,
-                    ),
-
-                    const SizedBox(height: 32),
-
-                    //email
-                    Center(
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints(maxWidth: 360),
-                        child: TextField(
-                          controller: _emailController,
-                          style: const TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            labelText: "Email",
-                            labelStyle: TextStyle(color: Colors.orange[400]),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.red.shade600,
-                                width: 2,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 5),
-
-                    // password
-                    Center(
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints(maxWidth: 360),
-                        child: TextField(
-                          controller: _passwordController,
-                          style: const TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            labelStyle: TextStyle(color: Colors.orange[400]),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.red.shade600,
-                                width: 2,
-                              ),
-                            ),
-                          ),
-                          obscureText: true,
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 15),
-
-                    // login button
-                    Center(
-                      child: SizedBox(
-                        width: 150,
-                        child: ElevatedButton(
-                          onPressed: login,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red.shade600, // button color
-                            foregroundColor: Colors.white, // text color
-                            minimumSize: const Size.fromHeight(
-                              48,
-                            ), // button size
-                          ),
-                          child: const Text("Login"),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 12),
-                  ],
+      backgroundColor: Colors.transparent,
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, _) {
+          return Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(
+                  cos(_controller.value * 2 * pi),
+                  sin(_controller.value * 2 * pi),
                 ),
+                end: Alignment(
+                  -cos(_controller.value * 2 * pi),
+                  -sin(_controller.value * 2 * pi),
+                ),
+                colors: const [Color(0xFFE61A4B), Color(0xFFFFEA21)],
               ),
+            ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          //logo
+                          Image.asset(
+                            'lib/assets/images/popcom logo.png',
+                            height: 120,
+                            fit: BoxFit.contain,
+                          ),
+
+                          const SizedBox(height: 32),
+
+                          //email
+                          Center(
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(maxWidth: 360),
+                              child: TextField(
+                                controller: _emailController,
+                                style: const TextStyle(color: Colors.black),
+                                decoration: InputDecoration(
+                                  labelText: "Email",
+                                  labelStyle: TextStyle(color: Colors.white),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.white70,
+                                      width: 2,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red.shade800,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 5),
+
+                          // password
+                          Center(
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(maxWidth: 360),
+                              child: TextField(
+                                controller: _passwordController,
+                                style: const TextStyle(color: Colors.black),
+                                decoration: InputDecoration(
+                                  labelText: "Password",
+                                  labelStyle: TextStyle(color: Colors.white),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.white70,
+                                      width: 2,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red.shade800,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                                obscureText: true,
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 15),
+
+                          // login button
+                          Center(
+                            child: SizedBox(
+                              width: 150,
+                              child: ElevatedButton(
+                                onPressed: login,
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white, // button color
+                                  foregroundColor:
+                                      Colors.red.shade500, // text color
+                                  minimumSize: const Size.fromHeight(
+                                    48,
+                                  ), // button size
+                                ),
+                                child: const Text("Login"),
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 12),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           );
         },
